37cf7646a09931ca2e64051e47361e0ba361126f
Tenpesto04 <jlchong2004@gmail.com>
2024-10-30 13:29:10 +0800

    d

--- User/shoping-cart.php
+++ User/shoping-cart.php
@@ -60,7 +60,7 @@ $cart_items_result = $conn->query($cart_items_query);
 	<title>Shoping Cart</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
+<!--================================================================================================-->	
 	<link rel="icon" type="image/png" href="images/icons/favicon.png"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
