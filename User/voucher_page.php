ac85afbe0e628d23e705d03007f4ec6b142ab4b1
Tenpesto04 <jlchong2004@gmail.com>
2024-11-13 22:25:17 +0800

    d

--- User/voucher_page.php
+++ User/voucher_page.php
@@ -141,7 +141,7 @@ $voucher_result = $conn->query($voucher_query);
 					<div class="menu-desktop">
 						<ul class="main-menu">
 							<li>
-								<a href="index.html">Home</a>
+								<a href="dashboard.php">Home</a>
 								<ul class="sub-menu">
 									<li><a href="index.html">Homepage 1</a></li>
 									<li><a href="home-02.html">Homepage 2</a></li>
@@ -150,7 +150,7 @@ $voucher_result = $conn->query($voucher_query);
 							</li>
 
 							<li>
-								<a href="product.html">Shop</a>
+								<a href="product.php">Shop</a>
 							</li>
 
 							<li class="label1" data-label1="hot">
@@ -162,11 +162,11 @@ $voucher_result = $conn->query($voucher_query);
 							</li>
 
 							<li>
-								<a href="about.html">About</a>
+								<a href="about.php">About</a>
 							</li>
 
 							<li>
-								<a href="contact.html">Contact</a>
+								<a href="contact.php">Contact</a>
 							</li>
 						</ul>
 					</div>	
