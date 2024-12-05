bf6dd5f00f09829c50b1858dbd2cd7dc655b3717
weikit1012 <1221203087@student.mmu.edu.my>
2024-12-05 17:53:49 +0800

    d

--- Admin/manage_order.php
+++ Admin/manage_order.php
@@ -96,7 +96,7 @@ include 'admin_sidebar.php';
     box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
     z-index: 10;
     margin-top: 5px;
-    margin-right:100px;
+    margin-right:10000px;
 }
 
 .btn-group:hover .dropdown-menu {
