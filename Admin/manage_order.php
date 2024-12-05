3950a6f2071d6c43e29c706fa84bddb1b3b1fc2c
weikit1012 <1221203087@student.mmu.edu.my>
2024-12-05 17:50:57 +0800

    d

--- Admin/manage_order.php
+++ Admin/manage_order.php
@@ -17,6 +17,8 @@ include 'admin_sidebar.php';
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/base/jquery-ui.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 
     <style>
         body {
