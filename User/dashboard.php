cb1451fe7c4e2d4bf8bbca85961914906516d623
Tenpesto04 <jlchong2004@gmail.com>
2024-12-06 10:44:21 +0800

    d

--- User/dashboard.php
+++ User/dashboard.php
@@ -504,7 +504,7 @@ $product_result = $connect->query($sql);
 						<a href="product.php?category_id=1" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 							<div class="block1-txt-child1 flex-col-l">
 								<span class="block1-name ltext-102 trans-04 p-b-8">
-									Women
+									Women's Bag
 								</span>
 
 								<span class="block1-info stext-102 trans-04">
@@ -529,7 +529,7 @@ $product_result = $connect->query($sql);
 						<a href="product.php?category_id=2" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 							<div class="block1-txt-child1 flex-col-l">
 								<span class="block1-name ltext-102 trans-04 p-b-8">
-									Men
+									Men's Bag
 								</span>
 
 								<span class="block1-info stext-102 trans-04">
