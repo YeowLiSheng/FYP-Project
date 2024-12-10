38e92357306a4e717671eed0c7b8cc0b6c18b3cd
weikit1012 <1221203087@student.mmu.edu.my>
2024-12-10 23:42:21 +0800

    d

--- Admin/adminreviewdetails.php
+++ Admin/adminreviewdetails.php
@@ -43,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $stmt->bind_param("si", $new_status, $review_id);
         $stmt->execute();
     }
-    header("Location: admin_reviewdetails.php?product_id=$product_id");
+    header("Location: adminreviewdetails.php?product_id=$product_id");
     exit();
 }
 ?>
