e81a9aac346baf063cf5b12d45bb188e8fac3e6c
weikit1012 <1221203087@student.mmu.edu.my>
2024-12-11 22:32:43 +0800

    d

--- Admin/adminreviewdetails.php
+++ Admin/adminreviewdetails.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include 'dataconnection.php';
 include 'admin_sidebar.php';
 
@@ -31,7 +32,8 @@ $reviews = $stmt->get_result();
 // 处理管理员操作
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $review_id = $_POST['review_id'];
-    $staff_id = $_SESSION['id']; // 使用 login.php 中的键名
+
+    $staff_id = $_SESSION['staff_id']; // 确保使用统一的键名
 
 
     if (isset($_POST['reply'])) {
