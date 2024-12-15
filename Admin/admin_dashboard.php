fdf6020424dd62a0187056f28541b361c971ac0f
weikit1012 <1221203087@student.mmu.edu.my>
2024-12-15 19:52:50 +0800

    d

--- Admin/admin_dashboard.php
+++ Admin/admin_dashboard.php
@@ -431,7 +431,11 @@ $lowStockProducts = getLowStockProducts($connect);
                 <tbody>
                     <?php foreach ($recentUsers as $user): ?>
                         <tr>
-                            <td><img class="user-image" src="../User/<?php echo htmlspecialchars($user['user_image']); ?>" alt="<?php echo htmlspecialchars($user['user_name']); ?>"></td>
+                        <td>
+                            <img class="user-image" 
+                                src="<?php 
+                                    $imagePath = "../User/" . htmlspecialchars($user['user_image']); 
+                                    echo (empty($user['user_image']) || !file_exists($imagePath)) ? "../User/images/user-image" : $imagePath; ?>" alt="<?php echo htmlspecialchars($user['user_name']); ?>"></td>       
                             <td><?php echo htmlspecialchars($user['user_name']); ?></td>
                             <td><?php echo htmlspecialchars($user['user_email']); ?></td>
                             <td><?php echo htmlspecialchars($user['user_join_time']); ?></td>
