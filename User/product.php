62a674aa19effbb3759da473fb193ad1c11cf692
weikit1012 <1221203087@student.mmu.edu.my>
2024-12-27 12:47:04 +0800

    d

--- User/product.php
+++ User/product.php
@@ -1013,43 +1013,52 @@ body {
 			</div>
 
 	        <div class="row isotope-grid">
-            <?php
-            // Display products dynamically
-            if ($product_result->num_rows > 0) {
-                while($product = $product_result->fetch_assoc()) {
-                    // Assign a class to each product based on its category_id
-                    echo '<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item category-' . $product['category_id'] . '">
-                            <div class="block2">
-                                <div class="block2-pic hov-img0">
-                                    <img src="images/' . $product['product_image'] . '" alt="IMG-PRODUCT">
-									<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1" 
-										data-id="' . $product['product_id'] . '">
-										Quick View
-								 	</a>
-                                </div>
-                                <div class="block2-txt flex-w flex-t p-t-14">
-                                    <div class="block2-txt-child1 flex-col-l ">
-                                        <a href="product-detail.php?id=' . $product['product_id'] . '" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">'
-                                        . $product['product_name'] . 
-                                        '</a>
-                                        <span class="stext-105 cl3">$' . $currency . ' $' . number_format($product_price, 2) .  '</span>
-                                    </div>
-                                    <div class="block2-txt-child2 flex-r p-t-3">
-                                        <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
-                                            <img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON">
-                                            <img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                          </div>';
-                }
-            } else {
-                echo "<p>No products found.</p>";
-            }
-            ?>
-        	</div>
+    <?php
+    // 确定用户选择的货币
+    session_start();
+    $currency = isset($_SESSION['currency']) ? $_SESSION['currency'] : 'usd'; // 默认 USD
+    $currency_field = 'product_price_' . strtolower($currency); // 动态选择数据库字段
 
+    // Display products dynamically
+    if ($product_result->num_rows > 0) {
+        while($product = $product_result->fetch_assoc()) {
+            // 动态获取产品价格
+            $product_price = $product[$currency_field];
+
+            // 显示产品
+            echo '<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item category-' . $product['category_id'] . '">
+                    <div class="block2">
+                        <div class="block2-pic hov-img0">
+                            <img src="images/' . $product['product_image'] . '" alt="IMG-PRODUCT">
+                            <a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1" 
+                                data-id="' . $product['product_id'] . '">
+                                Quick View
+                            </a>
+                        </div>
+                        <div class="block2-txt flex-w flex-t p-t-14">
+                            <div class="block2-txt-child1 flex-col-l ">
+                                <a href="product-detail.php?id=' . $product['product_id'] . '" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">'
+                                . $product['product_name'] . 
+                                '</a>
+                                <span class="stext-105 cl3">' 
+                                . strtoupper($currency) . ' ' . number_format($product_price, 2) .  
+                                '</span>
+                            </div>
+                            <div class="block2-txt-child2 flex-r p-t-3">
+                                <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+                                    <img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON">
+                                    <img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                  </div>';
+        }
+    } else {
+        echo "<p>No products found.</p>";
+    }
+    ?>
+</div>
 
 			<!-- Load more -->
 			<div class="flex-c-m flex-w w-full p-t-45">
