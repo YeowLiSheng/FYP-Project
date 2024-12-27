bcb8dc42ba67e20b4b7706d9da6d495f3c9d404e
weikit1012 <1221203087@student.mmu.edu.my>
2024-12-27 15:13:12 +0800

    d

--- User/product.php
+++ User/product.php
@@ -365,9 +365,6 @@ $output = ob_get_clean(); // Get any unexpected output
 if (!empty($output)) {
     error_log("Unexpected output: $output"); // Log unexpected output for debugging
 }
-
-$product_price = isset($row[$currency_field]) ? $row[$currency_field] : 0.00;
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -1277,8 +1274,10 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 						</h4>
 
 						<span class="mtext-106 cl2">
-                        
-                        $<?php strtoupper($currency) . ' ' . number_format($product_price, 2) ?>
+							$<?php 
+                            echo strtoupper($currency) . ' ' . number_format($product_price, 2); 
+                            
+                            ?>
 						</span>
 
 						<p class="stext-102 cl3 p-t-23">
