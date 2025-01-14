881271e386d66a5338a6f68a7bdfe31a6a9e2db5
weikit1012 <1221203087@student.mmu.edu.my>
2025-01-15 00:20:27 +0800

    d

--- User/checkout.php
+++ User/checkout.php
@@ -632,7 +632,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && !empty($_POST['cardHolderName']) &&
 							<div class="checkout-input-box">
     <span class="required">Postcode :</span>
     <input type="text" name="postcode" id="postcode" placeholder="12345" minlength="5" maxlength="5" 
-        pattern="\d{5}" title="Please enter exactly 5 digits" autocomplete="off" required>
+        pattern="\d{5}" title="Please enter exactly 5 digits number" autocomplete="off" required>
 </div>
 						</div>
 
