69f8b910a39da08ef2a07ba9c613ccd70c5f99c7
weikit1012 <1221203087@student.mmu.edu.my>
2025-01-14 20:08:48 +0800

    d

--- User/checkout.php
+++ User/checkout.php
@@ -565,8 +565,26 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 						</div>
 						<div class="checkout-flex">
 							<div class="checkout-input-box">
-								<span class="required">State :</span>
-								<input type="text" name="state" id="state" value="" required>
+							<span class="required">State :</span>
+							<select name="state" id="state" required>
+    							<option value="" disabled selected>Select a state</option>
+    							<option value="Johor">Johor</option>
+								<option value="Kelantan">Kelantan</option>
+								<option value="Kedah">Kedah</option>
+								<option value="Malacca">Malacca</option>
+            					<option value="Negeri Sembilan" >Negeri Sembilan</option>
+            					<option value="Pahang" >Pahang</option>
+            					<option value="Penang" >Penang</option>
+            					<option value="Perak" >Perak</option>
+            					<option value="Perlis" >Perlis</option>
+            					<option value="Selangor">Selangor</option>
+            					<option value="Terengganu" >Terengganu</option>
+            					<option value="Kuala Lumpur" >Kuala Lumpur</option>
+            					<option value="Labuan" >Labuan</option>
+            					<option value="Putrajaya" >Putrajaya</option>
+            					<option value="Sabah" >Sabah</option>
+            					<option value="Sarawak" >Sarawak</option>
+							</select>
 							</div>
 							<div class="checkout-input-box">
 								<span class="required">Postcode :</span>
