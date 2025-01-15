a68ea7378a775114a8f8e61a6c98684991b3c99b
weikit1012 <1221203087@student.mmu.edu.my>
2025-01-15 16:01:14 +0800

    d

--- User/checkout.php
+++ User/checkout.php
@@ -271,7 +271,7 @@ unset($_SESSION['errorMessages']);
     cursor: pointer;
     text-align: center;
     transition: background 0.3s;
-    margin-top: 60px;
+    margin-top: 62px;
 }
 
 .checkout-btn:hover {
