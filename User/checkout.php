970769abe97a5497461acf49bfc7fc793b91ed0d
weikit1012 <1221203087@student.mmu.edu.my>
2025-01-20 23:33:37 +0800

    d

--- User/checkout.php
+++ User/checkout.php
@@ -38,29 +38,27 @@ if ($address_result && mysqli_num_rows($address_result) > 0) {
 // Retrieve unique products with total quantity and price in the cart for the logged-in user
 $cart_query = "
     SELECT 
-        p.product_id,
-        p.product_name, 
-        p.product_price,
-		pv.variant_id,
-        pv.color, 
-        pv.size, 
-        pv.Quick_View1 AS product_image,
-        SUM(sc.qty) AS total_qty, 
-    	(IF(pv.promotion_id IS NOT NULL, pp.promotion_price, p.product_price) * SUM(sc.qty)) AS item_total_price
-		
-    FROM 
-        shopping_cart AS sc
-    JOIN 
-        product_variant AS pv ON sc.variant_id = pv.variant_id
-	LEFT JOIN 
-    	promotion_product AS pp ON pv.promotion_id = pp.promotion_id
-
-    JOIN 
-        product AS p ON pv.product_id = p.product_id
-    WHERE 
-        sc.user_id = '$user_id'
-    GROUP BY 
-        pv.variant_id
+    p.product_id,
+    p.product_name, 
+    IF(pv.promotion_id IS NOT NULL, pp.promotion_price, p.product_price) AS effective_price,
+    pv.variant_id,
+    pv.color, 
+    pv.size, 
+    pv.Quick_View1 AS product_image,
+    SUM(sc.qty) AS total_qty, 
+    (IF(pv.promotion_id IS NOT NULL, pp.promotion_price, p.product_price) * SUM(sc.qty)) AS item_total_price
+FROM 
+    shopping_cart AS sc
+JOIN 
+    product_variant AS pv ON sc.variant_id = pv.variant_id
+LEFT JOIN 
+    promotion_product AS pp ON pv.promotion_id = pp.promotion_id
+JOIN 
+    product AS p ON pv.product_id = p.product_id
+WHERE 
+    sc.user_id = '$user_id'
+GROUP BY 
+    pv.variant_id;
 ";
 
 $cart_result = mysqli_query($conn, $cart_query);
