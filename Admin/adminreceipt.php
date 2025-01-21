b99196f16776e17e561bb77aebe7e7f9adf7f40d
weikit1012 <1221203087@student.mmu.edu.my>
2025-01-21 13:37:28 +0800

    d

--- Admin/adminreceipt.php
+++ Admin/adminreceipt.php
@@ -128,7 +128,7 @@ $pdf->SetFont('Arial', 'B', 10);
 $pdf->Cell(10, 10, 'No.', 1, 0, 'C', true);
 $pdf->Cell(70, 10, 'Description', 1, 0, 'C', true);
 $pdf->Cell(20, 10, 'Color', 1, 0, 'C', true);
-$pdf->Cell(25, 10, 'Unit Price (RM)', 1, 0, 'C', true);
+$pdf->Cell(30, 10, 'Unit Price (RM)', 1, 0, 'C', true);
 $pdf->Cell(20, 10, 'Qty', 1, 0, 'C', true);
 $pdf->Cell(35, 10, 'Subtotal (RM)', 1, 1, 'C', true);
 
@@ -141,7 +141,7 @@ while ($detail = $details_result->fetch_assoc()) {
     $pdf->Cell(10, 8, $itemNumber, 1, 0, 'C');
     $pdf->Cell(70, 8, $detail['name'], 1, 0, 'C');
     $pdf->Cell(20, 8, $detail['color'], 1, 0, 'C'); // Product color
-    $pdf->Cell(25, 8, number_format($detail['unit_price'], 2), 1, 0, 'C');
+    $pdf->Cell(30, 8, number_format($detail['unit_price'], 2), 1, 0, 'C');
     $pdf->Cell(20, 8, $detail['quantity'], 1, 0, 'C');
     $pdf->Cell(35, 8, number_format($detail['total_price'], 2), 1, 1, 'C');
     $itemNumber++;
