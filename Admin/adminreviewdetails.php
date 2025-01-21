334450a5df8f04e753366671f64e8695afbedb40
weikit1012 <1221203087@student.mmu.edu.my>
2025-01-22 04:27:03 +0800

    d

--- Admin/adminreviewdetails.php
+++ Admin/adminreviewdetails.php
@@ -102,7 +102,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             document.addEventListener('DOMContentLoaded', function() {
                 Swal.fire({
                     icon: 'success',
-                    title: 'Operation Completed',
+                    title: 'Reply Updated',
                     text: '" . $success_message . "',
                     confirmButtonText: 'OK'
                 }).then((result) => {
