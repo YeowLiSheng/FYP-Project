5e6405d1e5197394e6a7794700ebf796ef5431e6
weikit1012 <1221203087@student.mmu.edu.my>
2025-01-22 14:37:52 +0800

    d

--- Admin/view_admin.php
+++ Admin/view_admin.php
@@ -179,7 +179,7 @@ $admin_id = $_SESSION['admin_id']; // Get the admin ID from the session
                                 Export:
                             </button>
                             <ul class="dropdown-menu">
-                                <li><button type="submit" class="dropdown-item" onclick="exportPDF()">PDF</button></li>
+                                <li><button type="submit" class="dropdown-item" name="admin_pdf"onclick="exportPDF()">PDF</button></li>
                                 <li><button type="submit" class="dropdown-item" name="admin_excel">CSV</button></li>
                             </ul>
                         </div>
