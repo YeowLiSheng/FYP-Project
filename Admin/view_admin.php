15ea436097af1b293e2c1db005bad27dd68163f9
weikit1012 <1221203087@student.mmu.edu.my>
2025-01-22 23:25:20 +0800

    d

--- Admin/view_admin.php
+++ Admin/view_admin.php
@@ -319,7 +319,7 @@ $admin_id = $_SESSION['admin_id']; // Get the admin ID from the session
     });
 
 
-    const rowsPerPage = 10;
+    const rowsPerPage = 1;
     const tableBody = document.getElementById('table-body');
     const paginationDiv = document.getElementById('pagination');
 
