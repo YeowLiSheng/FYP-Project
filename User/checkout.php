06d768307baa7a1561d2715cdbf08e9d6446fc02
weikit1012 <1221203087@student.mmu.edu.my>
2025-01-23 12:34:50 +0800

    d

--- User/checkout.php
+++ User/checkout.php
@@ -646,29 +646,17 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 			<!-- Topbar -->
 			<div class="top-bar">
 				<div class="content-topbar flex-sb-m h-full container">
-					<div class="left-top-bar" style="white-space: nowrap; overflow: hidden; display: block; flex: 1; max-width: calc(100% - 300px);">
-						<span style="display: inline-block; animation: marquee 20s linear infinite;">
-							Free shipping for standard order over $10000 <span style="padding-left: 300px;"></span> 
-							New user will get 10% discount!!!<span style="padding-left: 300px;"></span>
-							Get 5% discount for any purchasement above $5000 (code: DIS4FIVE)
-							<span style="padding-left: 300px;"></span> Free shipping for standard order over $10000 
-							<span style="padding-left: 300px;"></span> New user will get 10% discount!!! 
-							<span style="padding-left: 300px;"></span> Get 5% discount for any purchasement above $5000 (code: DIS4FIVE)
-						</span>
-						<style>
-							@keyframes marquee {
-								0% {
-									transform: translateX(0);
-								}
-								100% {
-									transform: translateX(-55%);
-								}
-							}
-						</style>
+					<div class="left-top-bar">
+						Free shipping for standard order over $100
 					</div>
 
 					<div class="right-top-bar flex-w h-full">
-				
+						<a href="#" class="flex-c-m trans-04 p-lr-25">
+							Help & FAQs
+						</a>
+
+
+
 						<a href="#" class="flex-c-m trans-04 p-lr-25">
 							EN
 						</a>
@@ -680,14 +668,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 
 
-                        <a href="Order.php?user=<?php echo $user_id; ?>" class="flex-c-m trans-04 p-lr-25">
-                            <?php
-								echo "HI '" . htmlspecialchars($user['user_name']);
-                            ?>
-                        </a>
+						<a href="edit_profile.php?edit_user=<?php echo $user_id; ?>" class="flex-c-m trans-04 p-lr-25">
+							<?php
+							echo "HI '" . htmlspecialchars($user["user_name"]);
+							?>
+						</a>
 
 
-                        <a href="log_out.php" class="flex-c-m trans-04 p-lr-25">
+						<a href="log_out.php" class="flex-c-m trans-04 p-lr-25">
 							LOG OUT
 						</a>
 
@@ -699,10 +687,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 			<div class="wrap-menu-desktop how-shadow1">
 				<nav class="limiter-menu-desktop container">
-					
-					<!-- Logo desktop -->		
-					<a href="dashboard.php" class="logo">
-						<img src="images/YLS2.jpg" alt="IMG-LOGO">
+
+					<!-- Logo desktop -->
+					<a href="#" class="logo">
+						<img src="images/icons/logo-01.png" alt="IMG-LOGO">
 					</a>
 
 					<!-- Menu desktop -->
@@ -710,14 +698,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 						<ul class="main-menu">
 							<li>
 								<a href="dashboard.php">Home</a>
+								<ul class="sub-menu">
+									<li><a href="index.html">Homepage 1</a></li>
+									<li><a href="home-02.html">Homepage 2</a></li>
+									<li><a href="home-03.html">Homepage 3</a></li>
+								</ul>
 							</li>
 
 							<li class="active-menu">
-								<a href="product.php">Shop</a>
-							</li>
-
-                            <li>
-								<a href="promotion.php">Promotion</a>
+								<a href="product.html">Shop</a>
 							</li>
 
 							<li class="label1" data-label1="hot">
@@ -725,29 +714,135 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 							</li>
 
 							<li>
-								<a href="blog.php">Blog</a>
+								<a href="blog.html">Blog</a>
 							</li>
 
 							<li>
-								<a href="about.php">About</a>
+								<a href="about.html">About</a>
 							</li>
 
 							<li>
-								<a href="contact.php">Contact</a>
+								<a href="contact.html">Contact</a>
 							</li>
 						</ul>
-					</div>	
+					</div>
 
 					<!-- Icon header -->
 					<div class="wrap-icon-header flex-w flex-r-m">
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
+							<i class="zmdi zmdi-search"></i>
+						</div>
 
-						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="<?php echo $distinct_count; ?>">
+						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart"
+							data-notify="2">
 							<i class="zmdi zmdi-shopping-cart"></i>
 						</div>
 
+						<a href="#" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti"
+							data-notify="0">
+							<i class="zmdi zmdi-favorite-outline"></i>
+						</a>
 					</div>
 				</nav>
-			</div>	
+			</div>
+		</div>
+
+		<!-- Header Mobile -->
+		<div class="wrap-header-mobile">
+			<!-- Logo moblie -->
+			<div class="logo-mobile">
+				<a href="index.html"><img src="images/icons/logo-01.png" alt="IMG-LOGO"></a>
+			</div>
+
+			<!-- Icon header -->
+			<div class="wrap-icon-header flex-w flex-r-m m-r-15">
+				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
+					<i class="zmdi zmdi-search"></i>
+				</div>
+
+				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart"
+					data-notify="2">
+					<i class="zmdi zmdi-shopping-cart"></i>
+				</div>
+
+				<a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti"
+					data-notify="0">
+					<i class="zmdi zmdi-favorite-outline"></i>
+				</a>
+			</div>
+
+			<!-- Button show menu -->
+			<div class="btn-show-menu-mobile hamburger hamburger--squeeze">
+				<span class="hamburger-box">
+					<span class="hamburger-inner"></span>
+				</span>
+			</div>
+		</div>
+
+
+		<!-- Menu Mobile -->
+		<div class="menu-mobile">
+			<ul class="topbar-mobile">
+				<li>
+					<div class="left-top-bar">
+						Free shipping for standard order over $100
+					</div>
+				</li>
+
+				<li>
+					<div class="right-top-bar flex-w h-full">
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							Help & FAQs
+						</a>
+
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							My Account
+						</a>
+
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							EN
+						</a>
+
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							USD
+						</a>
+					</div>
+				</li>
+			</ul>
+
+			<ul class="main-menu-m">
+				<li>
+					<a href="dashboard.php">Home</a>
+					<ul class="sub-menu-m">
+						<li><a href="index.html">Homepage 1</a></li>
+						<li><a href="home-02.html">Homepage 2</a></li>
+						<li><a href="home-03.html">Homepage 3</a></li>
+					</ul>
+					<span class="arrow-main-menu-m">
+						<i class="fa fa-angle-right" aria-hidden="true"></i>
+					</span>
+				</li>
+
+				<li>
+					<a href="product.php">Shop</a>
+				</li>
+
+				<li>
+					<a href="shoping-cart.php" class="label1 rs1" data-label1="hot">Features</a>
+				</li>
+
+				<li>
+					<a href="blog.html">Blog</a>
+				</li>
+
+				<li>
+					<a href="about.html">About</a>
+				</li>
+
+				<li>
+					<a href="contact.html">Contact</a>
+				</li>
+			</ul>
 		</div>
 
 		<!-- Modal Search -->
@@ -768,80 +863,70 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 	</header>
 
 	<!-- Cart -->
-	<!-- Cart -->
 	<div class="wrap-header-cart js-panel-cart">
-    <div class="s-full js-hide-cart"></div>
+		<div class="s-full js-hide-cart"></div>
 
-    <div class="header-cart flex-col-l p-l-65 p-r-25">
-        <div class="header-cart-title flex-w flex-sb-m p-b-8">
-            <span class="mtext-103 cl2">
-                Your Cart
-            </span>
+		<div class="header-cart flex-col-l p-l-65 p-r-25">
+			<div class="header-cart-title flex-w flex-sb-m p-b-8">
+				<span class="mtext-103 cl2">
+					Your Cart
+				</span>
 
-            <div class="fs-35 lh-10 cl2 p-lr-5 pointer hov-cl1 trans-04 js-hide-cart">
-                <i class="zmdi zmdi-close"></i>
-            </div>
-        </div>
+				<div class="fs-35 lh-10 cl2 p-lr-5 pointer hov-cl1 trans-04 js-hide-cart">
+					<i class="zmdi zmdi-close"></i>
+				</div>
+			</div>
 
-        <div class="header-cart-content flex-w js-pscroll">
-            <ul class="header-cart-wrapitem w-full" id="cart-items">
-                <?php
-                $total_price = 0;
-
-                if ($cart_result && $cart_result->num_rows > 0) {
-                    while ($cart_item = $cart_result->fetch_assoc()) {
-                        $total_price += $cart_item['item_total_price'];
-                        $product_image = htmlspecialchars($cart_item['product_image']);
-                        $item_name = htmlspecialchars($cart_item['item_name']);
-                        $item_price = number_format($cart_item['item_price'], 2);
-                        $total_qty = $cart_item['total_qty'];
-                        $item_total_price = number_format($cart_item['item_total_price'], 2);
-                        $color = htmlspecialchars($cart_item['color']);
-                        $size = htmlspecialchars($cart_item['size']);
-                        $item_link = "product-detail.php?id=" . $cart_item['item_id'];
-
-                        echo '
+			<div class="header-cart-content flex-w js-pscroll">
+				<ul class="header-cart-wrapitem w-full" id="cart-items">
+					<?php
+					// Display combined cart items
+					$total_price = 0;
+					if ($cart_items_result->num_rows > 0) {
+						while ($cart_item = $cart_items_result->fetch_assoc()) {
+							$total_price += $cart_item['total_price'];
+							echo '
                         <li class="header-cart-item flex-w flex-t m-b-12">
                             <div class="header-cart-item-img">
-                                <img src="images/' . $product_image . '" alt="IMG">
+                                <img src="images/' . $cart_item['product_image'] . '" alt="IMG">
                             </div>
                             <div class="header-cart-item-txt p-t-8">
-                                <a href="' . $item_link . '" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-                                    ' . $item_name . '
+                                <a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
+                                    ' . $cart_item['product_name'] . '
                                 </a>
                                 <span class="header-cart-item-info">
-                                    ' . $total_qty . ' x $' . $item_price . ' = $' . $item_total_price . '
-                                </span>
-                                <span class="header-cart-item-info">
-                                    Color: ' . $color . ' | Size: ' . $size . '
+                                    ' . $cart_item['total_qty'] . ' x $' . number_format($cart_item['product_price'], 2) . '
                                 </span>
                             </div>
                         </li>';
-                    }
-                } else {
-                    echo '<p>Your cart is empty.</p>';
-                }
-                ?>
-            </ul>
+						}
+					} else {
+						echo '<p>Your cart is empty.</p>';
+					}
+					?>
+				</ul>
 
-            <div class="w-full">
-                <div class="header-cart-total w-full p-tb-40">
-                    Total: $<span id="cart-total"><?php echo number_format($total_price, 2); ?></span>
-                </div>
+				<div class="w-full">
+					<div class="header-cart-total w-full p-tb-40">
+						Total: $<span id="cart-total"><?php echo number_format($total_price, 2); ?></span>
+					</div>
 
-                <div class="header-cart-buttons flex-w w-full">
-                    <a href="shoping-cart.php" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
-                        View Cart
-                    </a>
+					<div class="header-cart-buttons flex-w w-full">
+						<a href="shoping-cart.php"
+							class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
+							View Cart
+						</a>
 
-                    <a href="checkout.php" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-b-10">
-                        Check Out
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+						<a href="shoping-cart.html"
+							class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-b-10">
+							Check Out
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 
 					
 	<body class="checkout-root checkout-reset">
